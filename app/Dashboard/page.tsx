2d04d230992f116538bbe058bd8a1133daf89151
bruce <demoncharismatique@gmail.com>
2024-01-16 09:07:23 +0000

    first commit

---
+++ app/Dashboard/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+type Props = {};
+
+
+export default function page({}:Props) {
+    return <div>Bienvenue sur notre Dashboard</div>;
+}
---
+++ app/connexion/google/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+type Props = {};
+
+
+export default function page({}:Props) {
+    return <div>connexion avec Google</div>;
+}
