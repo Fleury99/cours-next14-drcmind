e98d666ea9d284d235115637446e51b5c265827f
bruce <demoncharismatique@gmail.com>
2024-01-27 16:31:27 +0000

    Data Fetching(recherche/afichage de données) et Systeme de Caching

--- app/articles/[id]/page.tsx
+++ app/articles/[id]/page.tsx
@@ -6,7 +6,27 @@ type Props = {
     }
 };
 
+const getData = async (id:number) => {
+    const response = await fetch(`http://localhost:4000/articles/${id}`);
+    const data = await response.json();
+    return data;
+    
+};
 
-export default function page({params}:Props) {
-    return <div>Bienvenue sur la Page de l'Article {params.id}</div>;
+export default async function page({ params }:Props) {
+    const article = await getData(params.id);
+    return (
+        <>  
+            <div className="container">
+                <h1 className="titre">{article.titre}</h1>
+                <p>{article.contenu}</p>
+                <p>{article.date}</p>
+                <p>{article.user_id}</p>
+            </div>
+            
+            <div className="container pt-5">
+                <span>{article.auteur}</span>
+            </div>    
+        </>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,32 @@ import Image from 'next/image'
 import Link from 'next/link'
 import Button from './ui/Button';
 
-export default function Home() {
-  console.log('Test de composant');
+// Création d'une fonction pour chercher les données
+const getData = async () => {
+
+  //Revalidation des données
+  const response = await fetch(`http://localhost:4000/articles`,{
+    next: {
+      revalidate: 0,
+    },
+  });
+  const data = await response.json();
+  return data;
+  
+};
+
+type Article = {
+  id: number;
+  titre: string;
+  contenu: string;
+  date: string;
+  user_id: number;
+};
+
+export default async function Home() {
+  const articles = await getData();
+
+  console.log(articles);
   return (
     <main className="">
       <h2>Bienvenue à tout le monde</h2> 
@@ -16,6 +40,24 @@ export default function Home() {
       <Link href="/connexion" >Connectez-vous</Link>
       <br/>
       <Button />
+
+      <div className="container">
+        {
+          //Affichage des données
+          articles.map((article:Article) => (
+            <div key={article.id} className="carte">
+              <Link href={`/articles/${article.id}`}>
+                {article.id}.{article.titre}
+              </Link>
+            </div>
+            
+          ) )
+        }
+      </div>
+
+      <Link href="/articles/create">Ajouter un article</Link>
     </main>
-  )
+
+    
+  );
 }
